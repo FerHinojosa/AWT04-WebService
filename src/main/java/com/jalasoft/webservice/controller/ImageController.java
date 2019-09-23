514bb4f9db1cd4e4e5a434191b627275d97c8d8e
Fernando Hinojosa <FernandoHinojosa@fundacion-jala.local>
2019-09-23 10:47:30 -0400

    Add author implement code conventions

--- src/main/java/com/jalasoft/webservice/controller/ImageController.java
+++ src/main/java/com/jalasoft/webservice/controller/ImageController.java
@@ -1,4 +1,9 @@
 package com.jalasoft.webservice.controller;
+/**
+ *
+ * @project WebService feature(OCRController)
+ * @author Fernando Hinojosa on 09/23/2019
+ */
 
 public class ImageController {
 }
