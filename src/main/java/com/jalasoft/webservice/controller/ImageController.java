0e9801d9674b49b45323d70bf217b87c8815d634
Fernando Hinojosa <FernandoHinojosa@fundacion-jala.local>
2019-09-25 10:02:22 -0400

    Implements the ImageController

--- src/main/java/com/jalasoft/webservice/controller/ImageController.java
+++ src/main/java/com/jalasoft/webservice/controller/ImageController.java
@@ -1,9 +1,51 @@
-package com.jalasoft.webservice.controller;
 /**
+ * Copyright (c) 2019 Jalasoft.
  *
- * @project WebService feature(OCRController)
- * @author Fernando Hinojosa on 09/23/2019
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jalasoft.
  */
+package com.jalasoft.webservice.controller;
 
+import com.jalasoft.webservice.model.ImageConvert;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+/**
+ * Implements ImageController with the Imageconvert classes
+ * @author Fernando Hinojosa on 09/24/2019
+ * @version v1.0
+ */
+@RestController
+@RequestMapping("/api/v1.0/image")
 public class ImageController {
+
+        @PostMapping
+        public String imageConvert (@RequestParam("file") MultipartFile file,
+                 @RequestParam (value = "dpi", defaultValue = "") int dpi,
+                 @RequestParam (value = "dest", defaultValue = "") String dest,
+                 @RequestParam(value = "ext", defaultValue = "") String ext) throws IOException {
+
+            String filePath = "C:\\Users\\fernandohinojosa\\Desktop\\" +
+            file.getOriginalFilename();
+            Path location = Paths.get(filePath);
+
+            Files.copy(file.getInputStream(), location, StandardCopyOption.REPLACE_EXISTING);
+
+            ImageConvert img = new ImageConvert();
+            String res = img.ImageConvert(filePath,dpi,dest,ext);
+
+            return res;
+        }
 }
