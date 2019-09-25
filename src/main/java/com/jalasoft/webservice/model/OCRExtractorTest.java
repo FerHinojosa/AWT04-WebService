f44f7e52166bdaee13097280fd97b67f1d3faea1
Raul Laredo Soria <RaulLaredo@Fundacion-jala.local>
2019-09-25 16:23:05 -0400

    Adding test case and updating build.gladle

---
+++ src/main/java/com/jalasoft/webservice/model/OCRExtractorTest.java
@@ -0,0 +1,12 @@
+package com.jalasoft.webservice.model;
+
+import junit.framework.TestCase;
+
+public class OCRExtractorTest extends TestCase {
+
+    public void extract() throws Exception {
+        int num = 2;
+        assertEquals(num, 2);
+    }
+
+}
