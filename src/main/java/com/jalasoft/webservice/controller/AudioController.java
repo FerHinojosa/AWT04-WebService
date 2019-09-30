0b40f2f1028672f0b8b434b9ab74ee5eeabeaaa6
Raul Laredo Soria <RaulLaredo@Fundacion-jala.local>
2019-09-30 13:56:27 -0400

    Updated audio controller with correction suggested

--- src/main/java/com/jalasoft/webservice/controller/AudioController.java
+++ src/main/java/com/jalasoft/webservice/controller/AudioController.java
@@ -1,6 +1,7 @@
 package com.jalasoft.webservice.controller;
 
 import com.jalasoft.webservice.model.*;
+import com.jalasoft.webservice.utils.Utils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
@@ -15,15 +16,16 @@ import java.io.IOException;
 public class AudioController {
     @PostMapping
     public Response convert (@RequestParam("file") MultipartFile file,
-                             @RequestParam("targetFile") MultipartFile targetFile,
+                             @RequestParam("nameFile") String nameFile,
                              @RequestParam(value = "codec", defaultValue = "libmp3lame") String codec,
                              @RequestParam(value = "bitRate", defaultValue = "64000") int bitRate,
                              @RequestParam(value = "channels", defaultValue = "1") int channels,
                              @RequestParam(value = "samplingRate", defaultValue = "22050") int samplingRate,
                              @RequestParam(value = "format", defaultValue = "mp3") String format
     ) throws IOException {
+        Utils utils = new Utils();
         String filePath = FileManager.getFilePath(file);
-        String fileTarget = FileManager.getFilePath(targetFile);
+        String fileTarget = utils.getTemp() + nameFile + "." + format ;
         AudioCriteria cri = new AudioCriteria();
         cri.setFilePath(filePath);
         cri.setTarget(fileTarget);
