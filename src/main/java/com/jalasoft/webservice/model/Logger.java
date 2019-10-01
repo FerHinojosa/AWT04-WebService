195153b7e46c1100e308345e6bea6ca81cc76f2e
Raul Laredo Soria <RaulLaredo@Fundacion-jala.local>
2019-10-01 15:56:24 -0400

    Starting Logger

---
+++ src/main/java/com/jalasoft/webservice/model/Logger.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2019 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jalasoft.
+ */
+package com.jalasoft.webservice.model;
+
+/**
+ * Implements the logger for logging the errors in the webservice.
+ *
+ * @author Raul Laredo
+ * @version v1.0
+ */
+public class Logger {
+}
