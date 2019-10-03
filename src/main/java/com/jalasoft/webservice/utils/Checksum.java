4cca6edfc930dc90ae8b20a7a4efed31a06078ee
Andy Bazualdo Quevedo <AndyBazualdo@Fundacion-jala.local>
2019-10-03 15:24:43 -0400

    class checksum

--- src/main/java/com/jalasoft/webservice/utils/Checksum.java
+++ src/main/java/com/jalasoft/webservice/utils/Checksum.java
@@ -1,3 +1,12 @@
+/**
+ * Copyright (c) 2019 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jalasoft.
+ */
 package com.jalasoft.webservice.utils;
 
 import java.io.FileInputStream;
@@ -7,12 +16,19 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 /**
- * @author lazaro on 9/30/2019.
- * @project WebService
+ * The class generates checksum code for files
+ * @author Andy Bazualdo on 9/23/19.
+ * @version v1.0
  */
 public class Checksum {
+    /**
+     * The method return a checksum string based on a file path
+     * @param filepath file send to generate checksum
+     * @return returns a string with generate checksum
+     * @throws IOException exception controls Input output errors
+     * @throws NoSuchAlgorithmException exception handler no such algorithm exceptions
+     */
     public String checksum(String filepath) throws IOException, NoSuchAlgorithmException {
-
         // file hashing with DigestInputStream
         MessageDigest md = MessageDigest.getInstance("MD5");
         try (DigestInputStream dis = new DigestInputStream(new FileInputStream(filepath), md)) {
