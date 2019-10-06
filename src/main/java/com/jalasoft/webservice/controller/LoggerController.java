6f2d8dada558093c20215769bd061cd1f6adc517
Raul M. Laredo Soria <raul.laredo@fundacion-jala.org>
2019-10-06 05:49:39 -0400

    Added dependencies, modified Loggercontroller, deleted failed test

--- src/main/java/com/jalasoft/webservice/controller/LoggerController.java
+++ src/main/java/com/jalasoft/webservice/controller/LoggerController.java
@@ -2,18 +2,22 @@ package com.jalasoft.webservice.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class LoggerController {
-    private static final Logger LOG = LoggerFactory.getLogger(LoggerController.class);
+
+    Logger logger = LoggerFactory.getLogger(LoggerController.class);
 
     @RequestMapping("/logger")
-    @ResponseBody
-    String home() {
-        LOG.warn("sending hello world response...");
-        return "Hello World!";
+    public String index() {
+        logger.trace("This is a TRACE message.");
+        logger.debug("This is a DEBUG message.");
+        logger.info("This is an INFO message.");
+        logger.warn("This is a WARN message.");
+        logger.error("You guessed it, an ERROR message.");
+
+        return "Welcome to Spring Logging! Check the console to see the log messages.";
     }
 }
