448d5962c2fc54ea52ccaa4182316ea14e32f8a0
Raul Laredo Soria <RaulLaredo@Fundacion-jala.local>
2019-10-09 18:02:50 -0400

    Update unit test

---
+++ src/test/java/com/jalasoft/webservice/model/AudioConvertTest.java
@@ -0,0 +1,13 @@
+package com.jalasoft.webservice.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AudioConvertTest {
+
+    @Test
+    public void convert() {
+
+    }
+}
