45a4f2aa8b2827ea50e3ffd34b058654064ccc49
Raul Laredo Soria <RaulLaredo@Fundacion-jala.local>
2019-10-10 16:12:10 -0400

    Adding more unit tests

--- src/test/java/com/jalasoft/webservice/controller/FileManagerTest.java
+++ src/test/java/com/jalasoft/webservice/controller/FileManagerTest.java
@@ -1,5 +1,30 @@
+package com.jalasoft.webservice.controller;
+
+import com.jalasoft.webservice.utils.Utils;
+import org.junit.Test;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 import static org.junit.Assert.*;
 
 public class FileManagerTest {
 
+    @Test
+    public void getFilePath() {
+        boolean thrown = false;
+        try{
+            MultipartFile file = null;
+            Utils utils = new Utils();
+            String filePath = utils.getTemp() + file.getOriginalFilename();
+            Path location = Paths.get(filePath);
+            Files.copy(file.getInputStream(), location, StandardCopyOption.REPLACE_EXISTING);
+        } catch (Exception e){
+            thrown= true;
+        }
+    }
 }
--- src/test/java/com/jalasoft/webservice/controller/OCRControllerTest.java
+++ src/test/java/com/jalasoft/webservice/controller/OCRControllerTest.java
@@ -1,5 +1,32 @@
+package com.jalasoft.webservice.controller;
+
+import com.jalasoft.webservice.model.DBManager;
+import com.jalasoft.webservice.model.OCRExtractor;
+import com.jalasoft.webservice.model.Response;
+import com.jalasoft.webservice.utils.Checksum;
+import org.junit.Test;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+
 import static org.junit.Assert.*;
 
 public class OCRControllerTest {
-
+    boolean thrown;
+    @Test
+    public void OCRExtractor() {
+        MultipartFile file = null;
+        try {
+            String filePath = FileManager.getFilePath(file);
+            Checksum checksum1 = new Checksum();
+            Response test = new Response();
+            DBManager db = new DBManager();
+            String checksumResult = checksum1.checksum(filePath);
+            String pathDb = "";
+            OCRExtractor ocr = new OCRExtractor();
+        }catch (Exception e){
+            thrown = true;
+        }
+    assertTrue(thrown);
+    }
 }
